cb167470331e35771edb06dc92c561446b28e57b
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2016-08-20 11:15:14 +0100

    Added files

--- Tests/Core/Source/ProjectTests/ProjectFileRepositoryTests.cpp
+++ Tests/Core/Source/ProjectTests/ProjectFileRepositoryTests.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2015-2016 Xavier Leclercq
+    Copyright (c) 2016 Xavier Leclercq
 
     Permission is hereby granted, free of charge, to any person obtaining a
     copy of this software and associated documentation files (the "Software"),
@@ -20,14 +20,10 @@
     IN THE SOFTWARE.
 */
 
-#include "ProjectTests.h"
 #include "ProjectFileRepositoryTests.h"
-#include "BakefileProjectTests.h"
+#include "CodeSmithy/Core/Projects/ProjectFileRepository.h"
 
-void AddProjectTests(TestHarness& theTestHarness)
+void AddProjectFileRepositoryTests(TestSequence& testSequence)
 {
-	TestSequence& projectTestSequence = theTestHarness.appendTestSequence("Project tests");
-
-    AddProjectFileRepositoryTests(projectTestSequence);
-	AddBakefileProjectTests(projectTestSequence);
+	TestSequence* repositoryTestSequence = new TestSequence("ProjectFileRepository tests", testSequence);
 }
--- Tests/Core/Source/ProjectTests/ProjectFileRepositoryTests.h
+++ Tests/Core/Source/ProjectTests/ProjectFileRepositoryTests.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2015-2016 Xavier Leclercq
+    Copyright (c) 2016 Xavier Leclercq
 
     Permission is hereby granted, free of charge, to any person obtaining a
     copy of this software and associated documentation files (the "Software"),
@@ -20,14 +20,13 @@
     IN THE SOFTWARE.
 */
 
-#include "ProjectTests.h"
-#include "ProjectFileRepositoryTests.h"
-#include "BakefileProjectTests.h"
+#ifndef _CODESMITHY_TEST_CORE_PROJECTTESTS_PROJECTFILEREPOSITORYTESTS_H_
+#define _CODESMITHY_TEST_CORE_PROJECTTESTS_PROJECTFILEREPOSITORYTESTS_H_
 
-void AddProjectTests(TestHarness& theTestHarness)
-{
-	TestSequence& projectTestSequence = theTestHarness.appendTestSequence("Project tests");
+#include "Ishiko/TestFramework/TestFrameworkCore.h"
 
-    AddProjectFileRepositoryTests(projectTestSequence);
-	AddBakefileProjectTests(projectTestSequence);
-}
+using namespace Ishiko::TestFramework;
+
+void AddProjectFileRepositoryTests(TestSequence& testSequence);
+
+#endif
