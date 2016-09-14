d4907633673fc442a11515d69a9885e5f7cc04f4
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2016-09-14 10:19:48 +0100

    Refactoring

--- UICore/Source/Settings/BakefileEditorSettings.cpp
+++ UICore/Source/Settings/BakefileEditorSettings.cpp
@@ -84,15 +84,7 @@ FontSettings& BakefileEditorSettings::fontSettings()
 
 void BakefileEditorSettings::load(pugi::xml_node node)
 {
-    pugi::xml_node useDefaultSettingsNode = node.child(useDefaultSettingsElementName);
-    if (std::string(useDefaultSettingsNode.child_value()) == "true")
-    {
-        m_useDefaultSettings = true;
-    }
-    else if (std::string(useDefaultSettingsNode.child_value()) == "false")
-    {
-        m_useDefaultSettings = false;
-    }
+    m_useDefaultSettings = XMLUtilities::getChildValueAsBool(node, useDefaultSettingsElementName, true);
     m_themeName = XMLUtilities::getChildValueAsString(node, themeNameElementName, "CodeSmithy Light Theme");
     m_overrideTheme = XMLUtilities::getChildValueAsBool(node, overrideThemeElementName, false);
     pugi::xml_node fontSettingsNode = node.child(fontSettingsElementName);
@@ -101,38 +93,10 @@ void BakefileEditorSettings::load(pugi::xml_node node)
 
 void BakefileEditorSettings::save(pugi::xml_node node) const
 {
-    pugi::xml_node useDefaultSettingsNode = node.child(useDefaultSettingsElementName);
-    if (!useDefaultSettingsNode)
-    {
-        useDefaultSettingsNode = node.append_child(useDefaultSettingsElementName);
-        if (m_useDefaultSettings)
-        {
-            useDefaultSettingsNode.append_child(pugi::node_pcdata).set_value("true");
-        }
-        else
-        {
-            useDefaultSettingsNode.append_child(pugi::node_pcdata).set_value("false");
-        }
-    }
-    else
-    {
-        if (m_useDefaultSettings)
-        {
-            useDefaultSettingsNode.text().set("true");
-        }
-        else
-        {
-            useDefaultSettingsNode.text().set("false");
-        }
-    }
+    XMLUtilities::setOrAppendChildNode(node, useDefaultSettingsElementName, m_useDefaultSettings);
     XMLUtilities::setOrAppendChildNode(node, themeNameElementName, m_themeName);
     XMLUtilities::setOrAppendChildNode(node, overrideThemeElementName, m_overrideTheme);
-    pugi::xml_node fontSettingsNode = node.child(fontSettingsElementName);
-    if (!fontSettingsNode)
-    {
-        fontSettingsNode = node.append_child(fontSettingsElementName);
-    }
-
+    pugi::xml_node fontSettingsNode = XMLUtilities::getOrAppendChildNode(node, fontSettingsElementName);
     m_fontSettings.save(fontSettingsNode);
 }
 
