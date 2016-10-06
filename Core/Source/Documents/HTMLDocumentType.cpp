ef2cd8fe420ccf64a57647cfa489bb199ebc9347
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2016-10-06 10:21:44 +0100

    Added files

---
+++ Core/Source/Documents/HTMLDocumentType.cpp
@@ -0,0 +1,56 @@
+/*
+    Copyright (c) 2015-2016 Xavier Leclercq
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    IN THE SOFTWARE.
+*/
+
+#include "Documents/HTMLDocumentType.h"
+#include "Documents/HTMLDocument.h"
+
+namespace CodeSmithy
+{
+
+HTMLDocumentType::HTMLDocumentType()
+    : DocumentType("HTML")
+{
+    appendExtension("html");
+    appendExtension("htm");
+}
+
+HTMLDocumentType::HTMLDocumentType(std::shared_ptr<CustomDocumentTypeData> customData)
+    : DocumentType("HTML")
+{
+    appendExtension("html");
+    appendExtension("htm");
+    setCustomData(customData);
+}
+
+std::shared_ptr<Document> HTMLDocumentType::createNewDocument(const DocumentId& id,
+                                                              const std::string& name) const
+{
+    return std::make_shared<HTMLDocument>(shared_from_this(), id, name);
+}
+
+std::shared_ptr<Document> HTMLDocumentType::createDocumentFromFile(const DocumentId& id,
+                                                                   const boost::filesystem::path& path) const
+{
+    return std::make_shared<HTMLDocument>(shared_from_this(), id, path.filename().generic_string(), path);
+}
+
+}
---
+++ Include/CodeSmithy/Core/Documents/HTMLDocument.h
@@ -0,0 +1,46 @@
+/*
+    Copyright (c) 2015-2016 Xavier Leclercq
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    IN THE SOFTWARE.
+*/
+
+#ifndef _CODESMITHY_CORE_DOCUMENTS_HTMLDOCUMENT_H_
+#define _CODESMITHY_CORE_DOCUMENTS_HTMLDOCUMENT_H_
+
+#include "Document.h"
+#include <boost/filesystem/path.hpp>
+
+namespace CodeSmithy
+{
+
+class HTMLDocument : public Document
+{
+public:
+    HTMLDocument(const std::shared_ptr<const DocumentType> type, const DocumentId& id,
+        const std::string& name);
+    HTMLDocument(const std::shared_ptr<const DocumentType> type, const DocumentId& id,
+        const std::string& name, const boost::filesystem::path& path);
+
+private:
+    void doSave(const boost::filesystem::path& path) const override;
+};
+
+}
+
+#endif
