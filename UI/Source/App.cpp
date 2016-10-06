5ac36f2cbddf1f831f84200e6371d791a3c01811
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2016-10-06 23:31:44 +0100

    Added files

--- UI/Source/App.cpp
+++ UI/Source/App.cpp
@@ -26,6 +26,7 @@
 #include "CodeSmithy/UIElements/Editors/BakefileCtrl.h"
 #include "CodeSmithy/UIElements/Editors/CMakeListsCtrl.h"
 #include "CodeSmithy/UIElements/Editors/CppFileCtrl.h"
+#include "CodeSmithy/UIElements/Editors/HTMLDocumentCtrl.h"
 #include "CodeSmithy/UIElements/Editors/PowerShellFileCtrl.h"
 #include "CodeSmithy/UIElements/Editors/PythonFileCtrl.h"
 #include "CodeSmithy/UIElements/Editors/WiXSourceFileCtrl.h"
@@ -57,7 +58,7 @@ App::App()
     m_documentTypes->add(std::make_shared<CMakeListsType>(std::make_shared<ControlCreationDocumentTypeData>(CMakeListsCtrl::Create)));
     m_documentTypes->add(std::make_shared<CodeSmithyProjectFileType>());
     m_documentTypes->add(std::make_shared<CppFileType>(std::make_shared<ControlCreationDocumentTypeData>(CppFileCtrl::Create)));
-    m_documentTypes->add(std::make_shared<HTMLDocumentType>());
+    m_documentTypes->add(std::make_shared<HTMLDocumentType>(std::make_shared<ControlCreationDocumentTypeData>(HTMLDocumentCtrl::Create)));
     m_documentTypes->add(std::make_shared<PowerShellFileType>(std::make_shared<ControlCreationDocumentTypeData>(PowerShellFileCtrl::Create)));
     m_documentTypes->add(std::make_shared<PythonFileType>(std::make_shared<ControlCreationDocumentTypeData>(PythonFileCtrl::Create)));
     m_documentTypes->add(std::make_shared<WiXSourceFileType>(std::make_shared<ControlCreationDocumentTypeData>(WiXSourceFileCtrl::Create)));
