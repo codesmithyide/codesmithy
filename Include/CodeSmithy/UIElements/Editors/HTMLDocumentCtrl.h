b3d88c7ead62392629ab28cd30f168be3cedd6af
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2016-10-06 10:36:08 +0100

    Added files

---
+++ Include/CodeSmithy/UIElements/Editors/HTMLDocumentCtrl.h
@@ -0,0 +1,63 @@
+/*
+    Copyright (c) 2015-2016 Xavier Leclercq
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    IN THE SOFTWARE.
+*/
+
+#ifndef _CODESMITHY_UIELEMENTS_EDITORS_HTMLDOCUMENTCTRL_H_
+#define _CODESMITHY_UIELEMENTS_EDITORS_HTMLDOCUMENTCTRL_H_
+
+#include "DocumentCtrl.h"
+#include "HTMLEditorCtrl.h"
+#include "CodeSmithy/UICore/Settings/AppSettings.h"
+#include "CodeSmithy/Core/Documents/HTMLDocument.h"
+
+namespace CodeSmithy
+{
+
+class HTMLDocumentCtrl : public DocumentCtrl
+{
+public:
+    static wxWindow* Create(wxWindow *parent, std::shared_ptr<Document> document,
+        const AppSettings& appSettings);
+
+    HTMLDocumentCtrl(wxWindow* parent, std::shared_ptr<Document> document,
+        const AppSettings& appSettings);
+
+    std::shared_ptr<const Document> document() const override;
+    std::shared_ptr<Document> document() override;
+
+    void cut() override;
+    void copy() override;
+    void paste() override;
+
+private:
+    void doSave(const boost::filesystem::path& path) override;
+
+    void onModified(wxStyledTextEvent& evt);
+
+private:
+    HTMLEditorCtrl* m_ctrl;
+    std::shared_ptr<HTMLDocument> m_document;
+
+};
+
+}
+
+#endif
---
+++ UIElements/Source/Editors/HTMLDocumentCtrl.cpp
@@ -0,0 +1,91 @@
+/*
+    Copyright (c) 2015-2016 Xavier Leclercq
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    IN THE SOFTWARE.
+*/
+
+#include "Editors/HTMLDocumentCtrl.h"
+#include <wx/sizer.h>
+
+namespace CodeSmithy
+{
+
+wxWindow* HTMLDocumentCtrl::Create(wxWindow *parent,
+                                   std::shared_ptr<Document> document,
+                                   const AppSettings& appSettings)
+{
+    return new HTMLDocumentCtrl(parent, document, appSettings);
+}
+
+HTMLDocumentCtrl::HTMLDocumentCtrl(wxWindow* parent,
+                                   std::shared_ptr<Document> document,
+                                   const AppSettings& appSettings)
+    : DocumentCtrl(parent), m_ctrl(0)
+{
+    m_ctrl = new HTMLEditorCtrl(this, appSettings);
+    m_ctrl->Bind(wxEVT_STC_MODIFIED, &HTMLDocumentCtrl::onModified, this);
+
+    m_document = std::dynamic_pointer_cast<HTMLDocument, Document>(document);
+    if (!m_document->filePath().empty())
+    {
+        m_ctrl->LoadFile(m_document->filePath().generic_string());
+    }
+
+    wxBoxSizer* topSizer = new wxBoxSizer(wxHORIZONTAL);
+    topSizer->Add(m_ctrl, 1, wxEXPAND);
+    SetSizer(topSizer);
+}
+
+std::shared_ptr<const Document> HTMLDocumentCtrl::document() const
+{
+    return m_document;
+}
+
+std::shared_ptr<Document> HTMLDocumentCtrl::document()
+{
+    return m_document;
+}
+
+void HTMLDocumentCtrl::cut()
+{
+    m_ctrl->Cut();
+}
+
+void HTMLDocumentCtrl::copy()
+{
+    m_ctrl->Copy();
+}
+
+void HTMLDocumentCtrl::paste()
+{
+    m_ctrl->Paste();
+}
+
+void HTMLDocumentCtrl::doSave(const boost::filesystem::path& path)
+{
+    m_ctrl->SaveFile(path.string());
+    m_document->setModified(false);
+}
+
+void HTMLDocumentCtrl::onModified(wxStyledTextEvent& evt)
+{
+    m_document->setModified(true);
+}
+
+}
