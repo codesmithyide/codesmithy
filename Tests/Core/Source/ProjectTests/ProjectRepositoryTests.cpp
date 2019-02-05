474dac8fd0f7191fbe6c2dc1ea17d4c3a9ac11de
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2019-02-05 15:42:20 +0000

    Test updates

--- Tests/Core/Source/ProjectTests/ProjectRepositoryTests.cpp
+++ Tests/Core/Source/ProjectTests/ProjectRepositoryTests.cpp
@@ -20,7 +20,7 @@
     IN THE SOFTWARE.
 */
 
-#include "ProjectFileRepositoryTests.h"
+#include "ProjectRepositoryTests.h"
 #include "CodeSmithy/Core/Projects/ProjectRepository.h"
 #include <boost/filesystem/operations.hpp>
 
--- Tests/Core/Source/ProjectTests/ProjectTests.cpp
+++ Tests/Core/Source/ProjectTests/ProjectTests.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2015-2018 Xavier Leclercq
+    Copyright (c) 2015-2019 Xavier Leclercq
 
     Permission is hereby granted, free of charge, to any person obtaining a
     copy of this software and associated documentation files (the "Software"),
@@ -26,7 +26,7 @@
 #include "BakefileProjectTypeTests.h"
 #include "CMakeProjectTypeTests.h"
 #include "ProjectTypesTests.h"
-#include "ProjectFileRepositoryTests.h"
+#include "ProjectRepositoryTests.h"
 #include "ProjectFileRepositoryNodeTests.h"
 #include "ProjectDescriptionTests.h"
 #include "ProjectLocationTests.h"
