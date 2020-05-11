cba2ef9909fdc26f62d1d18fb845301f257ac3d8
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2020-05-11 18:33:57 +0100

    Update main.cpp

--- CLI/Source/main.cpp
+++ CLI/Source/main.cpp
@@ -4,24 +4,52 @@
     See https://github.com/CodeSmithyIDE/CodeSmithy/blob/master/LICENSE.txt
 */
 
+// TODO: use Core instead of libgit2 directly
 #include <git2.h>
 #include <iostream>
 
-int main(int argc, char* argv[])
+namespace
+{
+
+// TODO: also bootstrap code in UI. Put common bootstrap code in Core.
+// TODO: error handling
+void Bootstrap()
 {
     git_libgit2_init();
 
-    git_repository* repository = nullptr;
-    int err = git_clone(&repository, "https://github.com/CodeSmithyIDE/CodeSmithy.git", "CodeSmithy", 0);
+    git_repository* project_repository = nullptr;
+    int err = git_clone(&project_repository, "https://github.com/CodeSmithyIDE/Project", "Project", 0);
     if (err < 0)
     {
         // TODO: check for null pointers
         char* msg = giterr_last()->message;
         // TODO: make nice wrapper for console output
-        std::cerr << (char)0x1B << "[91m"<< "ERROR: " << err << msg << (char)0x1B << "[0m" << std::endl;
+        std::cerr << (char)0x1B << "[91m" << "ERROR: " << err << msg << (char)0x1B << "[0m" << std::endl;
     }
+    git_repository_free(project_repository);
+
+    git_repository* codesmithy_repository = nullptr;
+    err = git_clone(&codesmithy_repository, "https://github.com/CodeSmithyIDE/CodeSmithy.git", "CodeSmithy", 0);
+    if (err < 0)
+    {
+        // TODO: check for null pointers
+        char* msg = giterr_last()->message;
+        // TODO: make nice wrapper for console output
+        std::cerr << (char)0x1B << "[91m" << "ERROR: " << err << msg << (char)0x1B << "[0m" << std::endl;
+    }
+    git_repository_free(codesmithy_repository);
 
     git_libgit2_shutdown();
+}
 
+}
+
+// TODO: add install command
+// TODO: add to path command
+// TODO: add boostrap command
+// TODO: error handling
+int main(int argc, char* argv[])
+{
+    Bootstrap();
     return 0;
 }
