b9e740ff8d9f909ef356db9503220c547c13faab
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2020-05-13 11:25:59 +0100

    Add code to build with Visual Studio 2017.

--- CLI/Source/main.cpp
+++ CLI/Source/main.cpp
@@ -5,6 +5,7 @@
 */
 
 // TODO: use Core instead of libgit2 directly
+#include "Ishiko/Process/ProcessCreator.h"
 #include <git2.h>
 #include <iostream>
 
@@ -28,6 +29,8 @@ void Bootstrap()
     }
     git_repository_free(project_repository);
 
+    // TODO: should get build instructions Project but that may introduce too many dependencies?
+
     git_repository* codesmithy_repository = nullptr;
     err = git_clone(&codesmithy_repository, "https://github.com/CodeSmithyIDE/CodeSmithy.git", "CodeSmithy", 0);
     if (err < 0)
@@ -40,6 +43,18 @@ void Bootstrap()
     git_repository_free(codesmithy_repository);
 
     git_libgit2_shutdown();
+
+    std::string commandLine = "C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/Common7/IDE/devenv.exe ";
+    commandLine.append("CodeSmithy/Core/Makefiles/VC15/CodeSmithyCore.sln");
+    commandLine.append(" /build ");
+    commandLine.append("Debug|x64");
+
+    Ishiko::Process::ProcessHandle processHandle;
+    err = Ishiko::Process::ProcessCreator::StartProcess(commandLine, processHandle);
+    if (err == 0)
+    {
+        processHandle.waitForExit();
+    }
 }
 
 }
@@ -48,6 +63,7 @@ void Bootstrap()
 // TODO: add to path command
 // TODO: add boostrap command
 // TODO: error handling
+// TODO: workspace init: extension csmws/csmprj
 int main(int argc, char* argv[])
 {
     Bootstrap();
