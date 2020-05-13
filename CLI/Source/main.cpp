7180bdc7165ffc9cd8f4593bc4d7eebe166e39db
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2020-05-13 13:04:36 +0100

    Extract to another directory

--- CLI/Source/main.cpp
+++ CLI/Source/main.cpp
@@ -4,8 +4,9 @@
     See https://github.com/CodeSmithyIDE/CodeSmithy/blob/master/LICENSE.txt
 */
 
-// TODO: use Core instead of libgit2 directly
+// TODO: add CI for this
 #include "Ishiko/Process/ProcessCreator.h"
+// TODO: use Core instead of libgit2 directly
 #include <git2.h>
 #include <iostream>
 
@@ -16,10 +17,14 @@ namespace
 // TODO: error handling
 void Bootstrap()
 {
+    // TODO: this should be a command line argument or something
+    // TODO: create dir
+    const char* workspaceDirectory = "D:/scratch/CodeSmithyCLI";
+
     git_libgit2_init();
 
     git_repository* project_repository = nullptr;
-    int err = git_clone(&project_repository, "https://github.com/CodeSmithyIDE/Project", "Project", 0);
+    int err = git_clone(&project_repository, "https://github.com/CodeSmithyIDE/Project", "D:/scratch/CodeSmithyCLI/Project", 0);
     if (err < 0)
     {
         // TODO: check for null pointers
@@ -32,12 +37,12 @@ void Bootstrap()
     // TODO: should get build instructions Project but that may introduce too many dependencies?
 
     git_repository* codesmithy_repository = nullptr;
-    err = git_clone(&codesmithy_repository, "https://github.com/CodeSmithyIDE/CodeSmithy.git", "CodeSmithy", 0);
+    err = git_clone(&codesmithy_repository, "https://github.com/CodeSmithyIDE/CodeSmithy.git", "D:/scratch/CodeSmithyCLI/CodeSmithy", 0);
     if (err < 0)
     {
         // TODO: check for null pointers
         char* msg = giterr_last()->message;
-        // TODO: make nice wrapper for console output
+        // TODO: make nice wrapper for console output -> Ishiko/Terminal
         std::cerr << (char)0x1B << "[91m" << "ERROR: " << err << msg << (char)0x1B << "[0m" << std::endl;
     }
     git_repository_free(codesmithy_repository);
@@ -45,7 +50,7 @@ void Bootstrap()
     git_libgit2_shutdown();
 
     std::string commandLine = "C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/Common7/IDE/devenv.exe ";
-    commandLine.append("CodeSmithy/Core/Makefiles/VC15/CodeSmithyCore.sln");
+    commandLine.append("D:/scratch/CodeSmithyCLI/CodeSmithy/CLI/Makefiles/VC15/CodeSmithyCLI.sln");
     commandLine.append(" /build ");
     commandLine.append("Debug|x64");
 
