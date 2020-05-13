3ab485624c852ed72dbcbef6a1d2af29ce8c2c91
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2020-05-13 14:20:22 +0100

    Add verbose mode

--- CLI/Source/main.cpp
+++ CLI/Source/main.cpp
@@ -15,14 +15,23 @@ namespace
 
 // TODO: also bootstrap code in UI. Put common bootstrap code in Core.
 // TODO: error handling
-void Bootstrap()
+void Bootstrap(bool verbose)
 {
     // TODO: this should be a command line argument or something
     // TODO: create dir
     const char* workspaceDirectory = "D:/scratch/CodeSmithyCLI";
 
+    if (verbose)
+    {
+        std::cout << "Workspace directory: " << workspaceDirectory << std::endl;
+    }
+
     git_libgit2_init();
 
+    if (verbose)
+    {
+        std::cout << "Cloning CodeSmithyIDE/Project in: " << "D:/scratch/CodeSmithyCLI/Project" << std::endl;
+    }
     git_repository* project_repository = nullptr;
     int err = git_clone(&project_repository, "https://github.com/CodeSmithyIDE/Project", "D:/scratch/CodeSmithyCLI/Project", 0);
     if (err < 0)
@@ -36,6 +45,10 @@ void Bootstrap()
 
     // TODO: should get build instructions Project but that may introduce too many dependencies?
 
+    if (verbose)
+    {
+        std::cout << "Cloning CodeSmithyIDE/CodeSmithy in: " << "D:/scratch/CodeSmithyCLI/CodeSmithy" << std::endl;
+    }
     git_repository* codesmithy_repository = nullptr;
     err = git_clone(&codesmithy_repository, "https://github.com/CodeSmithyIDE/CodeSmithy.git", "D:/scratch/CodeSmithyCLI/CodeSmithy", 0);
     if (err < 0)
@@ -49,6 +62,10 @@ void Bootstrap()
 
     git_libgit2_shutdown();
 
+    if (verbose)
+    {
+        std::cout << "Building CodeSmithyIDE/CodeSmithy/CLI/Makefiles/VC15/CodeSmithyCLI.sln" << std::endl;
+    }
     std::string commandLine = "C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/Common7/IDE/devenv.exe ";
     commandLine.append("D:/scratch/CodeSmithyCLI/CodeSmithy/CLI/Makefiles/VC15/CodeSmithyCLI.sln");
     commandLine.append(" /build ");
@@ -69,8 +86,18 @@ void Bootstrap()
 // TODO: add boostrap command
 // TODO: error handling
 // TODO: workspace init: extension csmws/csmprj
+// TODO: verbose mode
 int main(int argc, char* argv[])
 {
-    Bootstrap();
+    bool verbose = false;
+    if (argc > 1)
+    {
+        if (strncmp("-v", argv[1], 3) == 0)
+        {
+            verbose = true;
+        }
+    }
+
+    Bootstrap(verbose);
     return 0;
 }
