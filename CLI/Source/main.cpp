2142a3779f7bd20a5dd5dffb8701bb59d5bfd5de
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2020-05-14 12:25:07 +0100

    Refactoring

--- CLI/Source/main.cpp
+++ CLI/Source/main.cpp
@@ -6,6 +6,7 @@
 
 // TODO: add CI for this
 #include <Ishiko/Process/ProcessCreator.h>
+#include <Ishiko/FileSystem/Utilities.h>
 #include <Ishiko/Errors/Error.h>
 // TODO: use Core instead of libgit2 directly
 #include <git2.h>
@@ -25,8 +26,6 @@ void CloneRepository(const std::string& organization, const std::string& name, c
         std::cout << "Cloning " << organization << "/" << name << " in: " << targetDirectory << std::endl;
     }
 
-    git_libgit2_init();
-
     git_repository* project_repository = nullptr;
     int err = git_clone(&project_repository, repositoryURL.c_str(), targetDirectory.c_str(), 0);
     if (err < 0)
@@ -41,13 +40,38 @@ void CloneRepository(const std::string& organization, const std::string& name, c
     git_repository_free(project_repository);
 }
 
+void Build(const std::string& workspaceDirectory, bool verbose, Ishiko::Error& error)
+{
+    if (verbose)
+    {
+        std::cout << "Building CodeSmithyIDE/CodeSmithy/CLI/Makefiles/VC15/CodeSmithyCLI.sln" << std::endl;
+    }
+    std::string commandLine = "C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/Common7/IDE/devenv.exe ";
+    std::string makefilePath = workspaceDirectory + "/CodeSmithyIDE/CodeSmithy/CLI/Makefiles/VC15/CodeSmithyCLI.sln";
+    commandLine.append(makefilePath);
+    commandLine.append(" /build ");
+    commandLine.append("Debug|x64");
+
+    Ishiko::Process::ProcessHandle processHandle;
+    int err = Ishiko::Process::ProcessCreator::StartProcess(commandLine, processHandle);
+    if (err == 0)
+    {
+        processHandle.waitForExit();
+        if (processHandle.exitCode() != 0)
+        {
+            // TODO : error code + message
+            error.fail(-1);
+            // TODO: make nice wrapper for console output
+            std::cerr << (char)0x1B << "[91m" << "ERROR: " << processHandle.exitCode() << (char)0x1B << "[0m" << std::endl;
+        }
+    }
+}
+
 // TODO: also bootstrap code in UI. Put common bootstrap code in Core.
 // TODO: error handling
-void Bootstrap(bool verbose, Ishiko::Error& error)
+void Bootstrap(const std::string& workspaceDirectory, bool verbose, Ishiko::Error& error)
 {
-    // TODO: this should be a command line argument or something
-    // TODO: create dir
-    std::string workspaceDirectory = "D:/scratch/CodeSmithyCLI";
+    git_libgit2_init();
 
     if (verbose)
     {
@@ -70,50 +94,36 @@ void Bootstrap(bool verbose, Ishiko::Error& error)
 
     git_libgit2_shutdown();
 
-    if (verbose)
-    {
-        std::cout << "Building CodeSmithyIDE/CodeSmithy/CLI/Makefiles/VC15/CodeSmithyCLI.sln" << std::endl;
-    }
-    std::string commandLine = "C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/Common7/IDE/devenv.exe ";
-    commandLine.append("D:/scratch/CodeSmithyCLI/CodeSmithyIDE/CodeSmithy/CLI/Makefiles/VC15/CodeSmithyCLI.sln");
-    commandLine.append(" /build ");
-    commandLine.append("Debug|x64");
-
-    Ishiko::Process::ProcessHandle processHandle;
-    int err = Ishiko::Process::ProcessCreator::StartProcess(commandLine, processHandle);
-    if (err == 0)
-    {
-        processHandle.waitForExit();
-        if (processHandle.exitCode() != 0)
-        {
-            // TODO : error code + message
-            error.fail(-1);
-            // TODO: make nice wrapper for console output
-            std::cerr << (char)0x1B << "[91m" << "ERROR: " << processHandle.exitCode() << (char)0x1B << "[0m" << std::endl;
-        }
-    }
+    Build(workspaceDirectory, verbose, error);
 }
 
 }
 
-// TODO: add install command
-// TODO: add to path command
 // TODO: add boostrap command
 // TODO: workspace init: extension csmws/csmprj
 int main(int argc, char* argv[])
 {
     Ishiko::Error error(0);
 
+    bool bootstrap = true;
     bool verbose = false;
-    if (argc > 1)
+    for (int i = 1; i < argc; ++i)
     {
-        if (strncmp("-v", argv[1], 3) == 0)
+        const char* argument = argv[i];
+        if (strncmp("-v", argument, 3) == 0)
         {
             verbose = true;
         }
     }
 
-    Bootstrap(verbose, error);
+    // TODO: this should be a command line argument or something
+    // TODO: create dir
+    std::string workspaceDirectory = "D:/scratch/CodeSmithyCLI";
+
+    if (bootstrap)
+    {
+        Bootstrap(workspaceDirectory, verbose, error);
+    }
 
     if (error)
     {
