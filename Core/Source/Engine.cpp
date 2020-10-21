63bba80c0f16b45c9f1f314873ed4ea0558f59c7
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2020-10-21 12:17:34 +0100

    Update makefiles

--- Core/Source/Engine.cpp
+++ Core/Source/Engine.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2018-2019 Xavier Leclercq
+    Copyright (c) 2018-2020 Xavier Leclercq
 
     Permission is hereby granted, free of charge, to any person obtaining a
     copy of this software and associated documentation files (the "Software"),
@@ -21,6 +21,8 @@
 */
 
 #include "Engine.h"
+
+using namespace Ishiko;
 
 namespace CodeSmithy
 {
