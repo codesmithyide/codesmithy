d2609f1c1849dd9ea280dfb853b0002640c8c3c9
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-07-12 21:30:11 +0100

    Add CLI tests (#145)

--- cli/tests/Source/main.cpp
+++ cli/tests/Source/main.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2015-2019 Xavier Leclercq
+    Copyright (c) 2015-2021 Xavier Leclercq
 
     Permission is hereby granted, free of charge, to any person obtaining a
     copy of this software and associated documentation files (the "Software"),
@@ -21,7 +21,7 @@
 */
 
 #include "VisualStudioBuildTests/VisualStudioBuildTests.h"
-#include "Ishiko/TestFramework/TestFrameworkCore.h"
+#include <Ishiko/Tests/Core.h>
 
 using namespace Ishiko::Tests;
 
