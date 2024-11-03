bee42d7b4606bad7bf12a5d27315af736902224c
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2024-11-03 15:06:04 +0000

    Updates

--- core/tests/Source/ProjectTests/BakefileProjectTests.cpp
+++ core/tests/Source/ProjectTests/BakefileProjectTests.cpp
@@ -1,28 +1,9 @@
-/*
-    Copyright (c) 2015-2023 Xavier Leclercq
-
-    Permission is hereby granted, free of charge, to any person obtaining a
-    copy of this software and associated documentation files (the "Software"),
-    to deal in the Software without restriction, including without limitation
-    the rights to use, copy, modify, merge, publish, distribute, sublicense,
-    and/or sell copies of the Software, and to permit persons to whom the
-    Software is furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in
-    all copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-    IN THE SOFTWARE.
-*/
+// SPDX-FileCopyrightText: 2015-2024 Xavier Leclercq
+// SPDX-License-Identifier: MIT
 
 #include "BakefileProjectTests.h"
 #include "CodeSmithy/Core/Projects/Bakefile/BakefileProject.h"
-#include "CodeSmithy/Core/Projects/ProjectRepository.h"
+#include <CodeSmithy/BuildFiles.hpp>
 
 using namespace boost::filesystem;
 using namespace Ishiko;
@@ -50,7 +31,7 @@ void BakefileProjectTests::ConstructorTest2(Test& test)
 
     Ishiko::Error error;
 
-    CodeSmithy::ProjectRepository repository;
+    CodeSmithy::CodeSmithyProjectFileXMLRepository repository;
     repository.open(inputPath, error);
 
     ISHIKO_TEST_ABORT_IF(error);
@@ -75,7 +56,7 @@ void BakefileProjectTests::SaveTest1(Test& test)
     
     Ishiko::Error error;
 
-    CodeSmithy::ProjectRepository repository;
+    CodeSmithy::CodeSmithyProjectFileXMLRepository repository;
     repository.create(test.context().getOutputPath(outputName), error);
 
     ISHIKO_TEST_ABORT_IF(error);
