d78573cfd4a47419983e7f6b2b8dbdad883f8a5d
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2024-11-07 20:52:25 +0000

    Updates

--- cli/src/CommandLineSpecification.cpp
+++ cli/src/CommandLineSpecification.cpp
@@ -13,10 +13,15 @@ CommandLineSpecification::CommandLineSpecification()
     // TODO: how can I make this conditional on the project command?
     addPositionalOption(2, "subcommand", {Ishiko::CommandLineSpecification::OptionType::single_value});
 
-    // TODO: how can I make this conditional on the selected command?
+    // TODO: how can I make this conditional on the selected subcommand?
     addPositionalOption(3, "project-name", {Ishiko::CommandLineSpecification::OptionType::single_value});
+
+    // TODO: how can I make this conditional on the selected subcommand?
+    addPositionalOption(4, "file-path", {Ishiko::CommandLineSpecification::OptionType::single_value});
     
+    addNamedOption("input-file", {Ishiko::CommandLineSpecification::OptionType::single_value});
     addNamedOption("output-dir", {Ishiko::CommandLineSpecification::OptionType::single_value});
+    addNamedOption("repository-path", {Ishiko::CommandLineSpecification::OptionType::single_value});
+    addNamedOption("verbose", { Ishiko::CommandLineSpecification::OptionType::toggle });
     addNamedOption("work-dir", {Ishiko::CommandLineSpecification::OptionType::single_value});
-    addNamedOption("verbose", {Ishiko::CommandLineSpecification::OptionType::toggle});
 }
