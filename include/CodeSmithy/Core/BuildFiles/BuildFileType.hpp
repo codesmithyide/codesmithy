43e5ad5420adffd611d30d7af4967f7063ea0439
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2024-11-15 14:36:35 +0000

    Add files

---
+++ include/CodeSmithy/Core/BuildFiles/BuildFileType.hpp
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: 2017-2024 Xavier Leclercq
+// SPDX-License-Identifier: MIT
+
+#ifndef GUARD_CODESMITHYIDE_CODESMITHY_CORE_BUILDFILES_BUILDFILETYPE_HPP
+#define GUARD_CODESMITHYIDE_CODESMITHY_CORE_BUILDFILES_BUILDFILETYPE_HPP
+
+namespace CodeSmithy
+{
+    class BuildFileType
+    {
+    };
+}
+
+#endif
