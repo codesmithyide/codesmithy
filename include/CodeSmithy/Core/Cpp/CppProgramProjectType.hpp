07dbf211b993f995504117b581ecc0cc4a6e4b4a
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2024-11-16 17:15:56 +0000

    Updates

--- include/CodeSmithy/Core/Cpp/CppProgramProjectType.hpp
+++ include/CodeSmithy/Core/Cpp/CppProgramProjectType.hpp
@@ -8,6 +8,10 @@
 
 namespace CodeSmithy
 {
+    // TODO: The way the project types work is that this is a project that the user selects. From this project type we
+    // can get the build files that can support such a project. The build file will then have a list of file types it
+    // can support. The supported files is the intersection of file types supported by the CppProgramProjectType and
+    // the selected build file type.
     class CppProgramProjectType : public ProjectType
     {
     public:
