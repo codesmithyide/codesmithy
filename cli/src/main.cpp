1d625e0fea2c843b2b75a83a1d4434b8b78dcf1c
Xavier Leclercq <xavier@leclercq.dev>
2024-11-19 20:53:24 +0000

    Update code after changes to dependencies (#200)

--- cli/src/main.cpp
+++ cli/src/main.cpp
@@ -3,7 +3,7 @@
 
 #include "CommandLineSpecification.hpp"
 #include <CodeSmithy/BuildFiles.hpp>
-#include <CodeSmithy/BuildToolchains.h>
+#include <CodeSmithy/BuildToolchains.hpp>
 #include <CodeSmithy/CLIEngine.hpp>
 #include <CodeSmithy/Core.hpp>
 #include <CodeSmithy/VersionControl/Git/GitRepository.h>
