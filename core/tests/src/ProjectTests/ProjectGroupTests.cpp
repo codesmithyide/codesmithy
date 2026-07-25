35cd481c30ece5c54017953f14892583404662fe
Xavier Leclercq <xavier@leclercq.dev>
2026-07-25 18:49:02 +0100

    Updates

--- core/tests/src/ProjectTests/ProjectGroupTests.cpp
+++ core/tests/src/ProjectTests/ProjectGroupTests.cpp
@@ -12,8 +12,6 @@ ProjectGroupTests::ProjectGroupTests(const TestNumber& number, const TestContext
     : TestSequence(number, "ProjectGroup tests", context)
 {
     append<HeapAllocationErrorsTest>("Constructor test 1", ConstructorTest1);
-    append<HeapAllocationErrorsTest>("save test 3", SaveTest3);
-    append<HeapAllocationErrorsTest>("addProject test 1", AddProjectTest1);
 }
 
 void ProjectGroupTests::ConstructorTest1(Test& test)
@@ -21,70 +19,5 @@ void ProjectGroupTests::ConstructorTest1(Test& test)
     CodeSmithy::ProjectGroupType type;
     CodeSmithy::ProjectGroup project(type, "ProjectGroupTests_CreationTest1");
 
-    ISHIKO_TEST_PASS();
-}
-
-// Checks that calling save() twice works correctly
-void ProjectGroupTests::SaveTest3(Test& test)
-{
-    const char* output_name = "ProjectGroupTests_SaveTest3.csmthprj";
-
-    Ishiko::Error error;
-
-    CodeSmithy::CodeSmithyBuildFileXMLRepository repository;
-    repository.create(test.context().getOutputPath(output_name), error);
-    repository.getBuildFile(error)->addProject(output_name);
-
-    ISHIKO_TEST_ABORT_IF(error);
-
-    DiplodocusDB::XMLTreeDBNode projectNode = repository.getBuildFileRawNode(error);
-
-    ISHIKO_TEST_ABORT_IF(error);
-    ISHIKO_TEST_ABORT_IF_NOT(projectNode);
-
-    CodeSmithy::ProjectGroupType type;
-    CodeSmithy::ProjectGroup project(type, "MyProjectGroup");
-
-    project.addExternalProjectLink(CodeSmithy::ProjectLocation("location1"));
-    project.addExternalProjectLink(CodeSmithy::ProjectLocation("location2"));
-    project.addExternalProjectLink(CodeSmithy::ProjectLocation("location3"));
-
-    // We call save() twice on purpose
-    project.save(repository.db(), projectNode, error);
-    project.save(repository.db(), projectNode, error);
-    repository.close();
-
-    ISHIKO_TEST_FAIL_IF(error);
-    ISHIKO_TEST_FAIL_IF_OUTPUT_AND_REFERENCE_FILES_NEQ(output_name);
-    ISHIKO_TEST_PASS();
-}
-
-void ProjectGroupTests::AddProjectTest1(Test& test)
-{
-    const char* output_name = "ProjectGroupTests_AddProjectTest1.csmthprj";
-    
-    Ishiko::Error error;
-
-    CodeSmithy::CodeSmithyBuildFileXMLRepository repository;
-    repository.create(test.context().getOutputPath(output_name), error);
-    repository.getBuildFile(error)->addProject(output_name);
-
-    DiplodocusDB::XMLTreeDBNode projectNode = repository.getBuildFileRawNode(error);
-
-    ISHIKO_TEST_ABORT_IF(error);
-    ISHIKO_TEST_ABORT_IF_NOT(projectNode);
-    
-    CodeSmithy::ProjectGroupType type;
-    CodeSmithy::ProjectGroup project(type, "MyProjectGroup");
-
-    std::shared_ptr<CodeSmithy::ProjectGroup> childProject = std::make_shared<CodeSmithy::ProjectGroup>(type, "MyChildProject");
-    
-    project.addProject(childProject);
-
-    project.save(repository.db(), projectNode, error);
-    repository.close();
-
-    ISHIKO_TEST_FAIL_IF(error);
-    ISHIKO_TEST_FAIL_IF_OUTPUT_AND_REFERENCE_FILES_NEQ(output_name);
     ISHIKO_TEST_PASS();
 }
--- core/tests/src/ProjectTests/ProjectGroupTests.h
+++ core/tests/src/ProjectTests/ProjectGroupTests.h
@@ -32,8 +32,6 @@ public:
 
 private:
     static void ConstructorTest1(Ishiko::Test& test);
-    static void SaveTest3(Ishiko::Test& test);
-    static void AddProjectTest1(Ishiko::Test& test);
 };
 
 #endif
